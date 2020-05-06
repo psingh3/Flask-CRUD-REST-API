6838c9e3a633b255db3dcbdec21888ee24e97f42
Parminder Singh <parminder.yengusto@gmail.com>
2020-05-06 16:33:16 -0600

    Add Type hinting in resource store

--- resources/store.py
+++ resources/store.py
@@ -2,13 +2,13 @@ from flask_restful import Resource
 from models.store import StoreModel
 
 class Store(Resource):
-    def get(self, name):
+    def get(self, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
         return {'message': 'Store not found'}, 404
     
-    def post(self, name):
+    def post(self, name: str):
         if StoreModel.find_by_name(name):
             return {'message': f"A store with {name} already exists."}, 400
 
@@ -20,7 +20,7 @@ class Store(Resource):
 
         return store.json(), 201
 
-    def delete(self, name):
+    def delete(self, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             store.delete_from_db()
