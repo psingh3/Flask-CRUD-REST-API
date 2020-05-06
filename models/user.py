a341930aa1c177e9a465f49b4a0d16f8937b9277
Parminder Singh <parminder.yengusto@gmail.com>
2020-05-06 15:19:48 -0600

    Add Type hinting in User model

--- models/user.py
+++ models/user.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import sqlite3
 from db import db
 
@@ -8,32 +10,32 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         self.username = username
         self.password = password
 
-    def json(self):
+    def json(self) -> Dict:
         return {
             'id': self.id,
             'username': self.username
         }
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def delete_from_db(self):
-        db.session.delete(self)
-        db.session.commit()
-
     @classmethod
-    def find_by_username(cls, username):
+    def find_by_username(cls, username: str):
         return cls.query.filter_by(username=username).first()
 
     @classmethod
-    def find_by_id(cls, _id):
+    def find_by_id(cls, _id: int):
         return cls.query.filter_by(id=_id).first()
 
     @classmethod
     def find_all(cls):
         return cls.query.all()
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
